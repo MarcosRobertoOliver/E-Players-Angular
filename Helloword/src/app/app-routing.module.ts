ce8947827a22f47d8f217bbab6c3fefd843b46ee
Marcos Roberto <marco.2229roberto@gmail.com>
2022-08-24 10:38:29 -0400

    funcionamento perfeito APIs

--- Helloword/src/app/app-routing.module.ts
+++ Helloword/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { ProdutosComponent } from './views/produtos/produtos.component';
+
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "login", component: LoginComponent},
+  {path: "produtos", component: ProdutosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
